f183128ed6b9de1d947a94da7f3e8821412d059a
maubarrerag <mauricio.barrerag@gmail.com>
2017-02-26 21:37:45 -0600

    Raspberry Pi - Project 1

---
+++ project1/led.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+
+def Blink(speed):
+	while(True):
+		GPIO.output(7, True)
+		time.sleep(speed)
+		GPIO.output(7, False)
+		time.sleep(speed)
+
+Blink(5)
